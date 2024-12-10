ed5fb42569314f9dec1213e6cdffd7ffaa6d1fe1
Chenyuan Zhang <zcy921716@hotmail.com>
2024-12-10 20:47:43 +1100

    goal recognition

--- gr_pursuer/pursuer.py
+++ gr_pursuer/pursuer.py
@@ -6,8 +6,10 @@ from multigrid.core.actions import Action
 
 class Pursuer:
 
-    def __init__(self, agent):
+    def __init__(self, agent, goals, start_pos):
         self.agent = agent
+        self.goals = goals
+        self.start = start_pos
 
     def compute_action(self, observations):
 
@@ -22,7 +24,29 @@ class Pursuer:
             self.agent.state.terminated == True
             return Action.left
 
-        path = astar2d(pos, observations[1]["pos"], cost)
+
+        current_pos = observations[1]["pos"]
+
+        current_dis = len(astar2d(self.start, current_pos, cost)) - 1
+
+        probs = []
+        for goal in self.goals:
+            opt_cost = len(astar2d(self.start, goal, cost)) - 1
+            real_cost = current_dis + len(astar2d(current_pos, goal, cost)) - 1
+            prob = np.exp(-(real_cost - opt_cost))/(1+np.exp(-(real_cost - opt_cost)))
+            probs.append(prob)
+
+
+        total = sum(probs)
+        normalized_probs = [p / total for p in probs]
+
+        largest_index = np.argmax(normalized_probs)
+        infer_goal = self.goals[largest_index]
+        print(infer_goal)
+        
+
+        path = astar2d(pos, infer_goal, cost)
+
         next_pos = np.array(path[1])     
         dir_vec_ = next_pos - np.array(pos)
 
@@ -37,5 +61,7 @@ class Pursuer:
             else:
                 action = Action.left
 
-        return action
+        prob_dict = { g:p for g, p in zip(self.goals,normalized_probs)}
+        print(prob_dict)
+        return action, prob_dict
 
