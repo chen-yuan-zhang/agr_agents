7061b279a1d9db6b3559f501ccafc8589a627294
Chenyuan Zhang <zcy921716@hotmail.com>
2025-03-25 16:50:02 +1100

    pipeline ready

--- gr_pursuer/agents/target.py
+++ gr_pursuer/agents/target.py
@@ -22,67 +22,47 @@ class Target(BaseAgent):
         self.evasion_goal = None
         self.path = None
         self.agent.can_overlap = False
+        self.index = 0
 
-        if env.hidden_cost is not None:
-            self.hidden_cost = env.hidden_cost
+        self.hidden_cost = env.hidden_cost
 
         self.mode = MOVE2GOAL
 
 
     def compute_action(self, obs):
+        pos = tuple(obs["pos"])
+        dir = obs["dir"]
 
-        grid = obs["grid"][:, :, 0]
-        pos = list(obs["pos"])
-        dir = np.array(obs["dir"])
-        dir_vec = DIR_TO_VEC[dir]
-            
-        cost = self.hidden_cost if hasattr(self, "hidden_cost") else None
+        # if self.path is not None and pos + [int(dir)] in self.path:
+        #     index = self.path.index(pos + [int(dir)])
+        #     if index == len(self.path)-1:
+        #         self.path = None
 
-        if self.path is not None and pos + [int(dir)] in self.path:
-            index = self.path.index(pos + [int(dir)])
-            if index == len(self.path)-1:
-                self.path = None
+        #         if self.mode == EVADE:
+        #             self.mode = MOVE2GOAL
 
-                if self.mode == EVADE:
-                    self.mode = MOVE2GOAL
+        # if self.mode == EVADE:
+        #     # Choose a random position from the grid and move to that position
+        #     if self.evasion_goal is None:
+        #         self.evasion_goal = random.choice(np.argwhere(grid!=2))
+        #         self.path = astar2d(pos, self.evasion_goal, env, cost)
 
-        if self.mode == EVADE:
-            # Choose a random position from the grid and move to that position
-            if self.evasion_goal is None:
-                self.evasion_goal = random.choice(np.argwhere(grid!=2))
-                self.path = astar2d(pos, self.evasion_goal, env, cost)
+        #     if self.path is None or len(self.path)<=1:
+        #         self.mode = MOVE2GOAL
+        #         self.evasion_goal = None
+        #         self.path = None
 
-            if self.path is None or len(self.path)<=1:
-                self.mode = MOVE2GOAL
-                self.evasion_goal = None
-                self.path = None
-
-        if self.mode == MOVE2GOAL:
-            if self.path is None or pos not in self.path:
-                self.path = astar2d((pos, dir), self.goal, self.env, cost)  
-                index = 0  
+        # if self.mode == MOVE2GOAL:
+        #     if self.path is None or pos not in self.path:
+        #         self.path = astar2d((pos, dir), self.goal, self.env, cost)  
+        #         index = 0  
 
 
-                
-        if pos + [int(dir)] in self.path:
-            index = self.path.index(pos + [int(dir)])
-        else:
-            print(f"Error: pos {pos} not in path {self.path}")
 
-        if len(self.path) <= index+1:
-            print("Target: Path not processed")
-            return None
+        if self.path is None:
+            self.path = astar2d((pos, dir), self.goal, self.env, self.hidden_cost)
 
-        next_pos = self.path[index+1][:2]
-        next_dir = self.path[index+1][2]
 
-        if next_pos!=pos:
-            action = Action.forward
-        else:
-            if next_dir == (dir+1)%4:
-                action = Action.right
-            else:
-                action = Action.left
-
-        return action
+        self.index += 1
+        return self.path[self.index][0]
 
--- gr_pursuer/generator.py
+++ gr_pursuer/generator.py
@@ -8,42 +8,41 @@ from .agents.target import Target
 
 
 
-def main(nLayouts, nScenarios, enableHiddenCost, output):
+def main(nLayouts, nScenarios, enableHiddenCost, output, initial_distance):
 
     size = 32
-    dataset = pd.DataFrame(columns=["layout", "scenario", "observer_pos", "target_pos", "observer_dir", "target_dir", "goals", "target_goal", "cost"])
+    dataset = pd.DataFrame(columns=["layout", "scenario", "observer_pos", "target_pos", "observer_dir", "target_dir", "goals", "target_goal", "base_grid", "hidden_cost", "initial_distance"])
 
     for i in range(nLayouts):
-        env = GREnv(size=size, agent_view_size=5, base_grid=None, render_mode=None)
+        env = GREnv(size=size, agent_view_size=[5, 5], base_grid=None, render_mode=None, initial_distance = initial_distance, enable_hidden_cost=enableHiddenCost)
         env.reset()
         base_grid = env.base_grid
 
         for j in range(nScenarios):
-            env = GREnv(size=32, agent_view_size=5, base_grid=base_grid, render_mode=None)
+            env = GREnv(size=32, agent_view_size=[5, 5], base_grid=base_grid, render_mode=None, initial_distance = initial_distance, enable_hidden_cost=enableHiddenCost)
             env.reset()
 
             goals = env.goals
             target_goal = env.goal
 
-            cost = []
-            for goal in goals:
-                cost.append(len(astar2d((env.target.pos, env.target.dir), goal, env, env.hidden_cost)) - 1)
-
             local_data = pd.DataFrame([{"layout": i, "scenario": j, "observer_pos": env.observer.pos, "target_pos": env.target.pos, 
                                         "observer_dir": env.observer.dir, "target_dir": env.target.dir,
-                                        "goals": goals, "target_goal": target_goal, "cost": cost, "base_grid": base_grid.tolist(),
-                                        "hidden_cost": env.hidden_cost.tolist()}])
+                                        "goals": goals, "target_goal": target_goal, "base_grid": base_grid.tolist(),
+                                        "hidden_cost": env.hidden_cost.tolist(),
+                                        "initial_distance": initial_distance}])
             dataset = pd.concat([dataset, local_data], ignore_index=True)
+            print(i, j)
 
-    dataset.to_csv(output, index=False)
+    dataset.to_csv(str(initial_distance) + output, index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to generate scenarios")
     parser.add_argument("--nLayouts", type=int, default=10, help="Number of Layouts")
     parser.add_argument("--nScenarios", type=int, default=5, help="Number of scenarios per layout")
-    parser.add_argument("--enableHiddenCost", type=bool, default=False, help="Number of scenarios per layout")
+    parser.add_argument("--enableHiddenCost", type=bool, default=True, help="Number of scenarios per layout")
+    parser.add_argument("--init", type=int, default=3, help="Initial distance between observer and actor")
     parser.add_argument("--output", type=str, default="scenarios.csv", help="Dataset output directory")
 
     args = parser.parse_args()
 
-    main(args.nLayouts, args.nScenarios, args.enableHiddenCost, args.output)
+    main(args.nLayouts, args.nScenarios, args.enableHiddenCost, args.output, args.init)
